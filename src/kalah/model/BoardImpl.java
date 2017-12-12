be6a33b74b0ed55a2912ebbc2c66303b27c28dee
chrisonntag <sonntagc@fim.uni-passau.de>
2017-12-12 16:52:59 +0100

    Replace interface documentation with {@inheritDoc}

--- src/kalah/model/BoardImpl.java
+++ src/kalah/model/BoardImpl.java
@@ -8,12 +8,12 @@ import kalah.exceptions.IllegalMoveException;
 public class BoardImpl implements Board {
 
   /**
-   * The number of pits per player in the classical Kalah game.
+   * {@inheritDoc}
    */
   private int DEFAULT_PITS_PER_PLAYER = 6;
 
   /**
-   * The initial number of seeds in each pit.
+   * {@inheritDoc}
    */
   private int DEFAULT_SEEDS_PER_PIT = 3;
 
@@ -66,10 +66,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the player who should open or already has opened the game by the
-   * initial move.
-   *
-   * @return The player who makes the initial move.
+   * {@inheritDoc}
    */
   @Override
   public Player getOpeningPlayer() {
@@ -77,9 +74,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the player who owns the next game turn.
-   *
-   * @return The player who is allowed to make the next turn.
+   * {@inheritDoc}
    */
   @Override
   public Player next() {
@@ -98,18 +93,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Executes a human move. This method does not change the state of this
-   * instance, which is treated here as immutable. Instead, a new board/game
-   * is returned, which is a copy of {@code this} with the move executed.
-   *
-   * @param pit The number of the human pit whose contained seeds will be sowed
-   * counter-clockwise.
-   * @return A new board with the move executed. If the move is not valid, i.e.,
-   * the pit is empty, then {@code null} will be returned.
-   * @throws IllegalMoveException If the game is already over, or it is not the
-   * human's turn.
-   * @throws IllegalArgumentException If the provided parameter is invalid,
-   * e.g., the defined pit is not on the grid.
+   * {@inheritDoc}
    */
   @Override
   public Board move(int pit) {
@@ -193,13 +177,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Executes a machine move. This method does not change the state of this
-   * instance, which is treated here as immutable. Instead, a new board/game
-   * is returned, which is a copy of {@code this} with the move executed.
-   *
-   * @return A new board with the move executed.
-   * @throws IllegalMoveException If the game is already over, or it is not the
-   * machine's turn.
+   * {@inheritDoc}
    */
   @Override
   public Board machineMove() {
@@ -210,9 +188,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Sets the skill level of the machine.
-   *
-   * @param level The skill as a number, must be at least 1.
+   * {@inheritDoc}
    */
   @Override
   public void setLevel(int level) {
@@ -222,10 +198,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Checks if the game is over. Either one player has won or there is a tie,
-   * i.e., both players gained the same number of seeds.
-   *
-   * @return {@code true} if and only if the game is over.
+   * {@inheritDoc}
    */
   @Override
   public boolean isGameOver() {
@@ -233,14 +206,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Checks if the game state is won. Should only be called if
-   * {@link #isGameOver()} returns {@code true}.
-   *
-   * A game is won by a player if her own or the opponents pits are all empty,
-   * and the number of seeds in the own store plus the seeds in the own pits
-   * is more than the sum of seeds in the opponents pits and store.
-   *
-   * @return The winner or nobody in case of a tie.
+   * {@inheritDoc}
    */
   @Override
   public Player getWinner() {
@@ -266,10 +232,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the number of seeds of the specified pit index {@code pit}.
-   *
-   * @param pit The number of the pit.
-   * @return The pit's content.
+   * {@inheritDoc}
    */
   @Override
   public int getSeeds(int pit) {
@@ -277,10 +240,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the number of the source pit of the last executed move. A number of
-   * one of the stores is not possible.
-   *
-   * @return The ordering number of the last move's source pit.
+   * {@inheritDoc}
    */
   @Override
   public int sourcePitOfLastMove() {
@@ -288,10 +248,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the number of the target pit of the last executed move. The number
-   * of the move opponent's stores is not possible.
-   *
-   * @return The ordering number of the last move's target pit.
+   * {@inheritDoc}
    */
   @Override
   public int targetPitOfLastMove() {
@@ -299,9 +256,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the number of pits per player in this game.
-   *
-   * @return The number of pits per player.
+   * {@inheritDoc}
    */
   @Override
   public int getPitsPerPlayer() {
@@ -309,9 +264,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the initial number of seeds in each pit of the players.
-   *
-   * @return The initial number of seeds per pit.
+   * {@inheritDoc}
    */
   @Override
   public int getSeedsPerPit() {
@@ -319,11 +272,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the current number of the seeds of the player {@code player}. This
-   * is the sum of the seeds in her pits and in her store.
-   *
-   * @param player The player for which to sum up her seeds.
-   * @return The sum of the pits per player.
+   * {@inheritDoc}
    */
   @Override
   public int getSeedsOfPlayer(Player player) {
@@ -343,9 +292,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Creates and returns a deep copy of this board.
-   *
-   * @return A clone.
+   * {@inheritDoc}
    */
   @Override
   public BoardImpl clone() {
@@ -363,16 +310,7 @@ public class BoardImpl implements Board {
   }
 
   /**
-   * Gets the string representation of the current board with the numbers of
-   * contained seeds representing a pit. The upper line belongs to the machine
-   * and the lower to the human. The winning store is always the one to the
-   * right in the respective game direction of the player, i.e., the one with
-   * no opponent pit on the other line. Numbers are right aligned in columns
-   * of width digits of the maximum number in any pit or store. These columns
-   * are horizontally separated by an extra single white space.
-   *
-   * @return The string representation of the current game status with pits by
-   *         number of currently contained seeds.
+   * {@inheritDoc}
    */
   @Override
   public String toString() {
