1fe1c1fc678b66124ce5951841ff6306af54e7b5
chrisonntag <sonntagc@fim.uni-passau.de>
2018-01-10 00:48:11 +0100

    Add localScore attribute and fix updateScore method

--- src/kalah/controller/Node.java
+++ src/kalah/controller/Node.java
@@ -8,19 +8,24 @@ import kalah.model.players.Player;
 public class Node {
 
   private double score;
+  private double localScore;
   private int level;
   private Board board;
   private List<Node> children = new ArrayList<>();
 
-  public Node(Board board, List<Node> children, double score) {
+  public Node(Board board, List<Node> children, double score, int level) {
     this.board = board;
     this.score = score;
+    this.localScore = score;
     this.children = children;
+    this.level = level;
   }
 
-  public Node(Board board, double score) {
+  public Node(Board board, double score, int level) {
     this.board = board;
     this.score = score;
+    this.localScore = score;
+    this.level = level;
   }
 
   public List<Node> getChildren() {
@@ -37,6 +42,10 @@ public class Node {
 
   public double getScore() {
     return score;
+  }
+
+  public double getLocalScore() {
+    return localScore;
   }
 
   public void setScore(double score) {
@@ -66,21 +75,27 @@ public class Node {
    */
   public void updateScore() {
     if (this.getChildren() != null) {
+      // Calling this method recursively at the beginning ensures that
+      // the total score will be calculated at the leaves first wandering up
+      // to the root of the tree.
       this.getChildren().forEach(Node::updateScore);
+
       if (this.getChildren().size() == 0) {
         return;
       }
-      if (this.getBoard().next() == Player.MACHINE) {
-        this.setScore(this.getMax().getScore() + this.getScore());
-      } else {
-        this.setScore(this.getMin().getScore() + this.getScore());
+
+      if (this.getBoard().getOpeningPlayer() == Player.MACHINE) {
+        this.setScore(this.getMaxChild().getScore() + this.getScore());
+      } else if (this.getBoard().getOpeningPlayer() == Player.HUMAN) {
+        this.setScore(this.getMinChild().getScore() + this.getScore());
       }
     }
   }
 
-  public Node getMin() {
+  public Node getMinChild() {
+    List<Node> children = getChildren();
     Node minChild = children.get(0);
-    for (Node node : this.children) {
+    for (Node node : children) {
       if (node.getScore() < minChild.getScore()) {
         minChild = node;
       }
@@ -89,7 +104,8 @@ public class Node {
     return minChild;
   }
 
-  public Node getMax() {
+  public Node getMaxChild() {
+    List<Node> children = getChildren();
     Node maxChild = children.get(0);
     for (Node node : this.children) {
       if (node.getScore() > maxChild.getScore()) {
@@ -98,6 +114,11 @@ public class Node {
     }
 
     return maxChild;
+  }
+
+  @Override
+  public String toString() {
+    return "Level: " + level + ", Score: " + score;
   }
 
 }
